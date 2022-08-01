309097e0956ea31d027ef928d6cf3271ffa2b68d
Mixeration <mixeration@gmail.com>
2022-08-01 16:36:48 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SiriusSQL',
+    version='0.0.1SrSQL',
+    packages=['mysql, sys'],
+    url='',
+    license='MIT',
+    author='mixeration',
+    author_email='mixeration@gmail.com',
+    description=''
+)
